f0a82856e86674f6604f901de9c4885416a2de8a
kasimugur <kasiugur7@gmail.com>
2024-10-06 20:59:55 +0300

    error functions updated

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,22 +1,52 @@
 import { useEffect, useState } from "react"
 
 export default function Form() {
-  const [control, setControl] = useState(false);
-  const [value, setValue] = useState([]);
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState(null)
-const handleSubmit = (event) => {
-  event.preventDefault();
-  if (!firstName ||!lastName ||!email ||!password) {
-    setError('Please fill in all fields');
-  } else if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)) {
-    setError('Invalid email address');
-  }
-}
+  const [error, setError] = useState({})
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+  });
 
+  const formError = {
+    firstNameError: "First Name cannot be empty",
+    lastNameError: "Last Name cannot be empty",
+    emailError: "email cannot be empty",
+    passwordError: "Password cannot be empty"
+  }
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let newErrors = {}
+    if (!formData.firstName) {
+      newErrors.firstName = formError.firstNameError
+    }
+    if (!formData.lastName) {
+      newErrors.lastName = formError.lastNameError
+    }
+    if (!formData.email) {
+      newErrors.email = formError.emailError
+    }
+    if (!formData.password) {
+      newErrors.password = formError.passwordError
+    }
+    if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(formData.email)) {
+      newErrors.Invalid = "looks like this is not an email"
+    }
+    if (Object.keys(newErrors).length > 0) {
+      setError(newErrors);
+    } else {
+      setError({})
+    }
+
+  }
+
+  const inputHandleChange = (e) => {
+    const { name, value } = e.target
+    setFormData({ ...formData, [name]: value })
+  } 
 
   return (
     <>
@@ -24,37 +54,43 @@ const handleSubmit = (event) => {
         <div className="box">
           <p><span>Try it free 7 days</span>  then $20/mo. thereafter</p>
         </div>
-        {/* <div> */}
         <form className="form" onSubmit={handleSubmit} >
           <div className="input-group">
             <input
-            value={firstName} onChange={e => setFirstName(e.target.value)}
-            type="text" placeholder="First Name"/>
-            {!firstName  && <span className="error">First Name cannot be empty</span>}
+              value={formData.firstName} onChange={inputHandleChange}
+              name="firstName"
+              type="text" placeholder="First Name" />
+            {!formData.firstName && <span className="error">
+              {error.firstName}</span>}
           </div>
           <div className="input-group">
             <input
-            value={lastName} onChange={e => setLastName(e.target.value)}
-            type="text" placeholder="Last Name "/> 
-            {!lastName && <span className="error">Last Name cannot be empty</span>}
+              value={formData.lastName} onChange={inputHandleChange}
+              name="lastName"
+              type="text" placeholder="Last Name " />
+            {!formData.lastName && <span className="error">
+              {error.lastName}</span>}
           </div>
           <div className="input-group">
             <input
-             value={email} onChange={e => setEmail(e.target.value)}
-              type="email" placeholder="Email Address"/>
-              {!email && <span className="error">Email cannot be empty</span>}
-              { error === "Invalid email address" && <span className="error">{error} </span>}
+              value={formData.email} onChange={inputHandleChange}
+              name="email"
+              type="email" placeholder="Email Address" />
+            {!formData.email &&
+              <span className="error"> {error.email} </span> ||
+              <span className="error"> {error.Invalid} </span>}
           </div>
           <div className="input-group">
             <input
-            value={password} onChange={e => setPassword(e.target.value)}
-            type="password" placeholder="Password" />
-            {!password && <span className="error">Password cannot be empty</span>}
+              value={formData.password} onChange={inputHandleChange}
+              name="password"
+              type="password" placeholder="Password" />
+            {!formData.password && <span className="error">
+              {error.password}</span>}
           </div>
-            <button  type="submit" >Claim your free trial </button>
+          <button type="submit" >Claim your free trial </button>
           <p>By clicking the button, you are agreeing to our <a> Terms and Services </a></p>
         </form>
-        {/* </div> */}
       </div>
     </>
   )
